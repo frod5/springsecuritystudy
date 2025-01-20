6531d5ddbe057aca8a032245007e70932152da96
김선우 <frod5@kakao.com>
2025-01-20 23:10:01 +0900

    Spring security Study - logout 기능

--- src/main/java/io/security/springsecuritystudy/IndexController.java
+++ src/main/java/io/security/springsecuritystudy/IndexController.java
@@ -1,6 +1,7 @@
 package io.security.springsecuritystudy;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,5 +35,10 @@ public class IndexController {
 	@GetMapping("/anonymous")
 	public String anonymous() {
 		return "anonymous";
+	}
+
+	@GetMapping("/logoutSuccess")
+	public String logoutSuccess() {
+		return "logoutSuccess";
 	}
 }
--- src/main/java/io/security/springsecuritystudy/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritystudy/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,10 +17,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @EnableWebSecurity
 @Configuration
@@ -67,6 +72,34 @@ public class SecurityConfig {
 			.principal("guest")
 			.authorities("ROLE_GUEST")
 		);
+
+		//Log out
+		http.logout(logout -> logout
+			.logoutUrl("/logout")
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+			.logoutSuccessUrl("/logoutSuccess")
+			.logoutSuccessHandler(new LogoutSuccessHandler() {
+
+				@Override
+				public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+					Authentication authentication) throws IOException, ServletException {
+					response.sendRedirect("/logoutSuccess");
+				}
+			})
+			.deleteCookies("JSESSIONID", "remember-me")
+			.invalidateHttpSession(true)
+			.clearAuthentication(true)
+			.addLogoutHandler(new LogoutHandler() {
+				@Override
+				public void logout(HttpServletRequest request, HttpServletResponse response,
+					Authentication authentication) {
+					HttpSession session = request.getSession();
+					session.invalidate();
+					SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
+					SecurityContextHolder.getContextHolderStrategy().clearContext();
+				}
+			})
+			.permitAll());
 
 		return http.build();
 	}
